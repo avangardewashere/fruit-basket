edf31ad987d1e9f66fd6f51c9790786502c9032f
avangardewashere <avelpanaligan@gmail.com>
2024-12-10 18:03:33 +0800

    add/fruit

---
+++ models/Fruit.ts
@@ -0,0 +1,32 @@
+import mongoose,{Schema,Document,model}from 'mongoose';
+
+import {v4 as uuidv4} from 'uuid';
+
+export interface IFruit extends Document{
+	id:string;
+	fruitName:string;
+	details:string;
+}
+
+const FruitSchema:Schema<IFruit> = new Schema({
+	id:{
+		type:String,
+		default:uuidv4,
+		unique:true,
+		required:true,
+	},
+	fruitName:{
+		type:String,
+		required:true,
+	},
+	details:{
+		type:String,
+		required:true
+	},
+	
+},{timestamps:true})
+
+
+const Fruit = model<IFruit>('Fruit',FruitSchema)
+
+export default Fruit
