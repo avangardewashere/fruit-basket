8d4f13e5b73fe4fc54ec443ae18763e69f32b3a1
avangardewashere <avelpanaligan@gmail.com>
2024-12-10 18:46:58 +0800

    route/post-handler

--- app/api/fruit/route.ts
+++ app/api/fruit/route.ts
@@ -1,2 +1,10 @@
 import { NextResponse } from "next/server";
 import Fruits from "@/models/Fruit"
+
+export async function POST (req:Request){
+	try {
+		const body = await req.json();
+	} catch (e) {
+		console.log(e)
+	}
+}
