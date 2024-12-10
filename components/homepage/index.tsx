6b50db6496834a42e1b0e7a9421fed4d60da4b08
avangardewashere <avelpanaligan@gmail.com>
2024-12-10 17:33:22 +0800

    route/addFruit

--- components/homepage/index.tsx
+++ components/homepage/index.tsx
@@ -1,7 +1,28 @@
-export default function HomePage(){
-	return(
-		<div className="text-black">
-			hello
-		</div>
-	)
-}
+import { memo } from "react";
+import clsx from "clsx";
+import style from "./index.module.scss";
+import appleImg from "@/assets/img/apple.png"
+import Image from "next/image";
+import Link from "next/link";
+
+
+const Homepage = memo(function Homepage() {
+  return (
+    <div className={clsx(style.container)}>
+      <div>Homepage</div>
+
+      <div className={clsx(style.card)}>
+        <Image src={appleImg} alt="" />
+     
+        <div className={style.details}>
+          <h1>Apple</h1>
+          <span>A tasty Red Fruit</span>
+        </div>
+      </div>
+
+      <Link href={"/addFruit"}>Add a fruit</Link>
+    </div>
+  );
+});
+
+export default Homepage;
