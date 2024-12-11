bc79bab290d865089e1afb0e841f1eb9a093a10b
avangardewashere <avelpanaligan@gmail.com>
2024-12-11 11:06:48 +0800

    add/post-route

--- app/api/fruit/route.ts
+++ app/api/fruit/route.ts
@@ -1,10 +1,28 @@
 import { NextResponse } from "next/server";
-import Fruits from "@/models/Fruit"
+import Fruits from "@/models/Fruit";
+import connectMongoDB from "@/libs";
 
-export async function POST (req:Request){
-	try {
-		const body = await req.json();
-	} catch (e) {
-		console.log(e)
-	}
+export async function POST(req: Request) {
+  try {
+    const { fruit, details } = await req.json();
+
+    await connectMongoDB();
+
+    await Fruits.create({ fruit, details });
+
+    return NextResponse.json(
+      { message: "A new Fruit Item has been Added.", fruit: fruit },
+      { status: 201 }
+    );
+  } catch (e: any) {
+    console.log(e);
+
+    return NextResponse.json(
+      {
+        message: "Error in Creating a new Fruit item.",
+        error: e.message,
+      },
+      { status: 500 }
+    );
+  }
 }
